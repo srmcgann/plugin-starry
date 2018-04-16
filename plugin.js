a0117a51d12158aacc923acdabb01f1ce5cc6107
Scott <smcgann@cantelope.ml>
2018-04-16 11:40:46 -0700

    fixed loading event

--- plugin.js
+++ plugin.js
@@ -42,48 +42,46 @@ kiwi.plugin('starryPlugin', function(kiwi, log) {
   }
   Draw();
 
-  kiwi.on('irc.raw', function(command, event){
-    if(command === "JOIN" && !joined){
-      joined = true;
-      starControlsDiv = document.createElement("div");
-      starControlsDiv.className = "starControlsDiv";
-      starControlsDiv.style.position = "absolute";
-      starControlsDiv.style.right = "40px";
-      starControlsDiv.style.bottom = "80px";
-      starControlsDiv.style.background = kiwi.state.setting('starry.color');
-      starControlsDiv.style.padding = "10px";
-      starControlsDiv.style.border = "1px solid #aaa";
-      starControlsDiv.style.borderRadius = "6px";
-      starControlsDiv.style.width = "200px";
-      starControlsDiv.style.height = "200px";
-      starControlsDiv.style.display = "none";
-      starControlsDiv.style.zIndex = "100";
-      starControlsDiv.innerHTML = "<button onclick='document.getElementsByClassName(\"starControlsDiv\")[0].style.display = \"none\";document.getElementById(\"picker\").jscolor.hide();' style='position:absolute;right:5px;margin-top:-5px;border-radius:5px;background:#f88;'>X</button>";
-      starControlsDiv.innerHTML += `<div class="color"><input value="`+kiwi.state.setting('starry.color')+`" class="jscolor {width:169, padding:0, shadow:false, borderWidth:0, backgroundColor:'transparent', insetColor:'#fff', onFineChange:'setColor(this)'}" id="picker"></div>`;
-      starControlsDiv.innerHTML += `<div style="margin-top:110px">Star Speed:<input type="range" id="starSpeed" value="55"></div>`;
-      document.body.appendChild(starControlsDiv);
-      const starTool = document.createElement('i');
-      starTool.className = 'fa fa-star';
-      kiwi.addUi('input', starTool);
-      setColor = document.createElement("script");
-      setColor.innerHTML=`function setColor(picker){starColor=picker.toHEXString();document.getElementsByClassName("starControlsDiv")[0].style.background = starColor}`;
-      document.head.appendChild(setColor);
-      starColorPicker = document.createElement("script");
-      starColorPicker.src = "https://color-mixer.tk/jscolor.js";
-      document.head.appendChild(starColorPicker);
-      window.addEventListener("mouseup", function(){if(starControlsDiv.style.display === "block")document.getElementById('picker').jscolor.show()});
-      window.addEventListener("mousedown", function(){if(starControlsDiv.style.display === "block")document.getElementById('picker').jscolor.show()});
-      starTool.onclick = function(e){
-        e.preventDefault();
-        starClick = true;
-        if(starControlsDiv.style.display === "block"){
-          starControlsDiv.style.display = "none";
-          document.getElementById('picker').jscolor.hide()
-        }else{
-          starControlsDiv.style.display = "block";
-          document.getElementById('picker').jscolor.show();
-        }
+  if(!joined){
+    joined = true;
+    starControlsDiv = document.createElement("div");
+    starControlsDiv.className = "starControlsDiv";
+    starControlsDiv.style.position = "absolute";
+    starControlsDiv.style.right = "40px";
+    starControlsDiv.style.bottom = "80px";
+    starControlsDiv.style.background = kiwi.state.setting('starry.color');
+    starControlsDiv.style.padding = "10px";
+    starControlsDiv.style.border = "1px solid #aaa";
+    starControlsDiv.style.borderRadius = "6px";
+    starControlsDiv.style.width = "200px";
+    starControlsDiv.style.height = "200px";
+    starControlsDiv.style.display = "none";
+    starControlsDiv.style.zIndex = "100";
+    starControlsDiv.innerHTML = "<button onclick='document.getElementsByClassName(\"starControlsDiv\")[0].style.display = \"none\";document.getElementById(\"picker\").jscolor.hide();' style='position:absolute;right:5px;margin-top:-5px;border-radius:5px;background:#f88;'>X</button>";
+    starControlsDiv.innerHTML += `<div class="color"><input value="`+kiwi.state.setting('starry.color')+`" class="jscolor {width:169, padding:0, shadow:false, borderWidth:0, backgroundColor:'transparent', insetColor:'#fff', onFineChange:'setColor(this)'}" id="picker"></div>`;
+    starControlsDiv.innerHTML += `<div style="margin-top:110px">Star Speed:<input type="range" id="starSpeed" value="55"></div>`;
+    document.body.appendChild(starControlsDiv);
+    const starTool = document.createElement('i');
+    starTool.className = 'fa fa-star';
+    kiwi.addUi('input', starTool);
+    setColor = document.createElement("script");
+    setColor.innerHTML=`function setColor(picker){starColor=picker.toHEXString();document.getElementsByClassName("starControlsDiv")[0].style.background = starColor}`;
+    document.head.appendChild(setColor);
+    starColorPicker = document.createElement("script");
+    starColorPicker.src = "https://color-mixer.tk/jscolor.js";
+    document.head.appendChild(starColorPicker);
+    window.addEventListener("mouseup", function(){if(starControlsDiv.style.display === "block")document.getElementById('picker').jscolor.show()});
+    window.addEventListener("mousedown", function(){if(starControlsDiv.style.display === "block")document.getElementById('picker').jscolor.show()});
+    starTool.onclick = function(e){
+      e.preventDefault();
+      starClick = true;
+      if(starControlsDiv.style.display === "block"){
+        starControlsDiv.style.display = "none";
+        document.getElementById('picker').jscolor.hide()
+      }else{
+        starControlsDiv.style.display = "block";
+        document.getElementById('picker').jscolor.show();
       }
     }
-  });
+  }
 });
